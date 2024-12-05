840a1ac7f40adb1045797f7ba4a603757fefb639
aadilsal <salmanaadil52@gmail.com>
2024-12-05 09:30:10 +0500

    CHAT FIXED2

--- pojek/settings.py
+++ pojek/settings.py
@@ -51,7 +51,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [('127.0.0.1',
-                        6380)],
+                        6379)],
         },
     },
 }
